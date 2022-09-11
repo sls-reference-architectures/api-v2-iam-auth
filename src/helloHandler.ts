c2252db2d2a5159ecbee14ff32bb57ba6aced7f2
Seth Orell <seth.orell@gmail.com>
2022-09-11 16:42:05 -0500

    Update message

--- src/helloHandler.ts
+++ src/helloHandler.ts
@@ -1,7 +1,7 @@
-import Logger from '@dazn/lambda-powertools-logger';
+import Logger from "@dazn/lambda-powertools-logger";
 
 const handler = async (event: any) => {
-  Logger.debug('Hello! I am protected by IAM', { event });
+  Logger.debug("Hello! I am protected by IAM.", { event });
 };
 
 export default handler;
