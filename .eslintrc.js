d2c6db3437ff9e98365647a12df61f6f32d44d9c
Seth Orell <seth.orell@gmail.com>
2022-09-11 16:40:04 -0500

    Add lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,57 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
   env: {
     jest: true,
+    node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base', 'airbnb-typescript/base', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['no-only-tests'],
+  root: true,
   rules: {
-    'no-use-before-define': 0,
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        ts: 'never',
-      },
-    ],
+    'import/extensions': 0,
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['off'],
+    'no-only-tests/no-only-tests': 'error',
   },
   settings: {
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true,
-      },
-    },
+    'import/resolver': 'node',
   },
 };
+
+// module.exports = {
+//   env: {
+//     jest: true,
+//     node: true,
+//   },
+//   extends: [
+//     'eslint:recommended',
+//     'plugin:@typescript-eslint/eslint-recommended',
+//     'plugin:@typescript-eslint/recommended',
+//     'airbnb-base',
+//   ],
+//   parser: '@typescript-eslint/parser',
+//   plugins: [
+//     '@typescript-eslint',
+//   ],
+//   root: true,
+//   rules: {
+//     'no-use-before-define': 0,
+//     'import/extensions': [
+//       'error',
+//       'ignorePackages',
+//       {
+//         ts: 'never',
+//       },
+//     ],
+//   },
+//   settings: {
+//     'import/resolver': {
+//       typescript: {
+//         alwaysTryTypes: true,
+//       },
+//     },
+//   },
+// };
